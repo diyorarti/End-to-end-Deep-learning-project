e7432ae0e74aec565359c91e829550a7704dde2e
diyorarti <diyorarti@example.com>
2024-07-20 07:48:46 +0500

    folder structure

---
+++ template.py
@@ -0,0 +1,58 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+project_name = "Xray"
+
+
+
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/common.py",
+    f"{project_name}/cloud_storage/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/ml/__init__.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logging/logger.py",
+    f"{project_name}/constant/__init__.py",
+    "test/integrationtest/__init__.py",
+    "test/unittest/__init__.py",
+    "bentofile.yaml",
+    "init_setup.sh",
+    "main.py",
+    "app.py",
+    "requirements.txt",
+    "setup.py",
+    "setup.cfg",
+    "tox.ini",
+    "research/trials.ipynb",
+    ".github/workflows/ci.yaml",
+    ".github/workflows/main.yaml"
+
+]
+
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
